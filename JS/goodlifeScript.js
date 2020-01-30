fca1eac6874e610ff4117969f408e9fd23bd65c0
sicel <sicelubuhlen@gmail.com>
2020-01-30 16:41:38 +0200

    the next Good Life Pages

--- JS/goodlifeScript.js
+++ JS/goodlifeScript.js
@@ -1,3 +1,6 @@
-const go = document.querySelector('#Page2');
-const main = document.querySelector('.Page1');
-const next = document.addEventListener('.BssubWed');
+const back = document.querySelector('.back1');
+
+back.addEventListener('click',()=>{
+    window.location = 'goodlife.html';
+})
+
