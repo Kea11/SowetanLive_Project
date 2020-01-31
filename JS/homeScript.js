680c44889787b80de68d3d1ca9a1fbc3fd251515
Dineo <d.motingoe@gmail.com>
2020-01-31 15:38:30 +0200

    cookie wrapper added to the homepage

--- JS/homeScript.js
+++ JS/homeScript.js
@@ -1,11 +1,9 @@
 const close = document.querySelector('.close');
-const nav = document.getElementById("tag");
 
 close.style.display = 'none';
 
 function myFunction() {
     var open = document.getElementById("myLinks");
-    nav.style.display = 'none';
     close.style.display = 'block';
     if (open.style.display === "block") {
       open.style.display = "none";
@@ -16,6 +14,36 @@ function myFunction() {
 
 close.addEventListener('click',()=>{
 window.location = "index.html"
+});
+
+
+// code for the pop up 
+ const bod = document.querySelectorAll('body');
+ const wrapper = document.querySelector('.popUpWrapper');
+// console.log('wrapper');
+const closePopUp = document.querySelector('.closePop');
+
+const popUpWrapper = document.querySelector('.popUpWrapper');
+
+const AcceptButton = document.querySelector('.accept');
+
+window.addEventListener('pageshow', () => {
+     wrapper.style.display = 'block';
+});
+
+closePopUp.addEventListener('click', () =>{
+    wrapper.style.display = 'none';
+    // window.location = "index.html";
+});
+
+popUpWrapper.addEventListener('pageshow', e =>{
+    if(e.target.className === 'popUpWrapper'){
+        popUpWrapper.style.display = 'none';
+    }
+});
+
+AcceptButton.addEventListener('click', () =>{
+  wrapper.style.display = 'none';
 });
 
 
