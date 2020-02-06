faeddf6caea0a9797089880d9bea6ff1c6781cec
Keabetswe Mogotsi <Keabetswe Mogotsi>
2020-02-06 16:46:08 +0200

    Pages finessed

--- JS/opinionScript.js
+++ JS/opinionScript.js
@@ -1,7 +1,24 @@
-const firstPg = document.querySelector('.op1');
-const secondPg = document.querySelector('.op2');
-const clicked = document.getElementById('letpic');
+// const firstPg = document.querySelector('.op1');
+// const secondPg = document.querySelector('.op2');
+// const clicked = document.getElementById('letpic');
 
-clicked.addEventListener('click', () => {
-    window.location.href = "clickedPg.html";
+// clicked.addEventListener('click', () => {
+//     window.location.href = "clickedPg.html";
+// });
+
+
+const subLog = document.querySelector('.user');
+
+subLog.addEventListener('click', () => {
+    window.location.href = 'SubscribePg.html';
 });
+
+
+const pressed = document.getElementById('bar');
+const opened = document.getElementById('open')
+
+pressed.addEventListener('click',()=>{
+   opened.style.backgroundColor = 'white';
+});
+
+
