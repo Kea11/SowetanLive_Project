d6c1b6dd8ae1b6b4a55261491655dfaf89b5e780
sicel <sicelubuhlen@gmail.com>
2020-02-06 15:40:29 +0200

    GoodLife with navbar

--- JS/goodlifeScript.js
+++ JS/goodlifeScript.js
@@ -1,9 +1,12 @@
 const back = document.querySelector('.back1');
+const pressed = document.getElementById('bar');
+const opened = document.getElementById('open')
 
-back.addEventListener('click',()=>{
-    window.location = 'goodlife.html';
+pressed.addEventListener('click',()=>{
+   opened.style.backgroundColor = 'white';
 });
 
-
-
+// back.addEventListener('click',()=>{
+//     window.location = 'goodlife.html';
+// });
 
