cf28d2c78b8c2d0ef7d7fbd6daad24498c212b2d
sicel <sicelubuhlen@gmail.com>
2020-02-19 16:35:49 +0200

    API done on the Finance Page

--- JS/goodlifeScript.js
+++ JS/goodlifeScript.js
@@ -1,55 +1,72 @@
-// const pressed = document.querySelector('.navbar');
-// const pressPg = document.querySelector('#navbar')
-// const opened = document.getElementById('bar');
-// const close = document.querySelector('.close');
-// const logo = document.querySelector('.log')
+const pressed = document.querySelector('.navbar');
+const pressPg = document.querySelector('#navbar')
+const opened = document.getElementById('bar');
+const close = document.querySelector('.close');
+const logo = document.querySelector('.log')
 
 
-// pressed.addEventListener('click',()=>{
-//    opened.style.backgroundColor = 'white';
-//    opened.style.display = 'block';
-//    close.style.display = 'block';
-//    pressed.style.display = 'none';
-//    logo.style.display = 'none';
-// });
+pressed.addEventListener('click',()=>{
+   opened.style.backgroundColor = 'white';
+   opened.style.display = 'block';
+   close.style.display = 'block';
+   pressed.style.display = 'none';
+   logo.style.display = 'none';
+});
 
-// // pressPg.addEventListener('click',()=>{
-// //     opened.style.backgroundColor = 'white';
-// //     opened.style.display = 'block';
-// //     close.style.display = 'block';
-// //     pressed.style.display = 'none';
-// //     logo.style.display = 'none';
-// //  });
+// pressPg.addEventListener('click',()=>{
+//     opened.style.backgroundColor = 'white';
+//     opened.style.display = 'block';
+//     close.style.display = 'block';
+//     pressed.style.display = 'none';
+//     logo.style.display = 'none';
+//  });
 
-// close.addEventListener('click', ()=> {
-// window.location = 'goodlifeWithLifestyleBlogs.html'
-// });
+close.addEventListener('click', ()=> {
+window.location = 'goodlifeWithLifestyleBlogs.html'
+});
 
-// const back = document.querySelector('.back1');
+const back = document.querySelector('.back1');
 
-// back.addEventListener('click',()=>{
-//     window.history.back();
-// });
+back.addEventListener('click',()=>{
+    window.location= 'goodlifeWithLifestyleBlogs.html';
+});
 
-// const subLog = document.querySelector('.user');
+const subLog = document.querySelector('.user');
 
-// subLog.addEventListener('click', () => {
-//     window.location.href = 'SubscribePg.html';
-// });
+subLog.addEventListener('click', () => {
+    window.location.href = 'SubscribePg.html';
+});
 
 
-const articleContent = document.querySelector('#Quote1');
+const articleContent = document.querySelector('.UK');
+const articleContent1 = document.querySelector('.US');
+const articleContent2 = document.querySelector('.Can');
+const articleContent3 = document.querySelector('.Sin');
+const articleContent4 = document.querySelector('.Aus');
 
-const key = "7944fcc06cmshcdd98a8104b33c1p17390bjsn1fef96bbd219"
 
-const base = "https://andruxnet-random-famous-quotes.p.rapidapi.com/?cat=famous&count=10"
+const base = "https://api.exchangeratesapi.io/latest"
 
-const get = async () =>{
-	const response = await fetch(base + key);
+const getRates = async () =>{
+	const response = await fetch(base);
 	const data = await response.json();
 	return data;
 };
-
-get()
-.then(data => console.log(data))
+getRates()
+.then(data => {
+	Finance = data;
+	articleContent.innerHTML = `<td class="UK" >${Finance.rates.GBP}</td> </p>`
+})
+.then(data => {
+	articleContent1.innerHTML = `<td class="UK" >${Finance.rates.USD}</td> </p>`
+})
+.then(data => {
+	articleContent2.innerHTML = `<td class="UK" >${Finance.rates.CAD}</td> </p>`
+})
+.then(data => {
+	articleContent3.innerHTML = `<td class="UK" >${Finance.rates.SGD}</td> </p>`
+})
+.then(data => {
+	articleContent4.innerHTML = `<td class="UK" >${Finance.rates.AUD}</td> </p>`
+})
 .catch(err => console.log(err));
