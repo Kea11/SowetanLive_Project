2f04d9221199cfb69c126260ab51305a34dbfb60
Keabetswe Mogotsi <Keabetswe Mogotsi>
2020-03-04 15:49:56 +0200

    Forms adjusted

--- JS/opinionScript.js
+++ JS/opinionScript.js
@@ -52,3 +52,43 @@ const test = document.querySelector('.drop');
 //     foot.style.display = 'block';
 //     signUp.style.display = 'none';
 // });
+
+
+// Log out
+
+const dropDowns = document.querySelector('.arrowLogOut');
+const show = document.querySelector('#myDropdown');
+
+dropDowns.addEventListener('click',()=>{
+    show.style.display = "block";
+});
+
+show.addEventListener('click', ()=>{
+    window.location = "logIn.html";
+});
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+const button = document.querySelector('.RegClass');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+button.addEventListener('click',()=>{
+  window.location.href = 'goodlifeWithLifestyleBlogs.html'
+});
