dba7d3ce324ae2b9ef7b9a1683c7066757f38417
sicel <sicelubuhlen@gmail.com>
2020-03-04 10:15:40 +0200

    The Sign In/ Register Pop Up

--- JS/goodlifeScript.js
+++ JS/goodlifeScript.js
@@ -1,46 +1,49 @@
-const pressed = document.querySelector('#navbar');
-const opened = document.getElementById('bar');
-const close = document.querySelector('.close');
-const logo = document.querySelector('.log');
+// const pressed = document.querySelector('#navbar');
+// const opened = document.getElementById('bar');
+// const close = document.querySelector('.close');
+// const logo = document.querySelector('.log');
 
-const back = document.querySelector('.back1');
-
-
-
-const articleContent = document.querySelector('.UK');
-const articleContent1 = document.querySelector('.US');
-const articleContent2 = document.querySelector('.Can');
-const articleContent3 = document.querySelector('.Sin');
-const articleContent4 = document.querySelector('.Aus');
+// const back = document.querySelector('.back1');
 
 
-const base = "https://api.exchangeratesapi.io/latest";
-
-const getRates = async () =>{
-	const response = await fetch(base);
-	const data = await response.json();
-	return data;
-};
-
-getRates()
-.then(data => {  
-	Finance = data;
-	articleContent.innerHTML = `<td class="UK" >${Finance.rates.GBP.toFixed(2)}</td> </p>`;
-	articleContent1.innerHTML = `<td class="UK" >${Finance.rates.USD.toFixed(2)}</td> </p>`;
-	articleContent2.innerHTML = `<td class="UK" >${Finance.rates.CAD.toFixed(2)}</td> </p>`;
-	articleContent3.innerHTML = `<td class="UK" >${Finance.rates.SGD.toFixed(2)}</td> </p>`;
-	articleContent4.innerHTML = `<td class="UK" >${Finance.rates.AUD.toFixed(2)}</td> </p>`;
-})
-.catch(err => console.log(err));
 
 
-pressed.addEventListener('click', ()=>{
-   opened.style.backgroundColor = 'white';
-   opened.style.display = 'block';
-   close.style.display = 'block';
-   pressed.style.display = 'none';
-   logo.style.display = 'none';
-});
+
+
+// const articleContent = document.querySelector('.UK');
+// const articleContent1 = document.querySelector('.US');
+// const articleContent2 = document.querySelector('.Can');
+// const articleContent3 = document.querySelector('.Sin');
+// const articleContent4 = document.querySelector('.Aus');
+
+
+// const base = "https://api.exchangeratesapi.io/latest";
+
+// const getRates = async () =>{
+// 	const response = await fetch(base);
+// 	const data = await response.json();
+// 	return data;
+// };
+
+// getRates()
+// .then(data => {  
+// 	Finance = data;
+// 	articleContent.innerHTML = `<td class="UK" >${Finance.rates.GBP.toFixed(2)}</td> </p>`;
+// 	articleContent1.innerHTML = `<td class="UK" >${Finance.rates.USD.toFixed(2)}</td> </p>`;
+// 	articleContent2.innerHTML = `<td class="UK" >${Finance.rates.CAD.toFixed(2)}</td> </p>`;
+// 	articleContent3.innerHTML = `<td class="UK" >${Finance.rates.SGD.toFixed(2)}</td> </p>`;
+// 	articleContent4.innerHTML = `<td class="UK" >${Finance.rates.AUD.toFixed(2)}</td> </p>`;
+// })
+// .catch(err => console.log(err));
+
+
+// pressed.addEventListener('click', ()=>{
+//    opened.style.backgroundColor = 'white';
+//    opened.style.display = 'block';
+//    close.style.display = 'block';
+//    pressed.style.display = 'none';
+//    logo.style.display = 'none';
+// });
 
 // pressPg.addEventListener('click',()=>{
 //     opened.style.backgroundColor = 'white';
@@ -50,14 +53,33 @@ pressed.addEventListener('click', ()=>{
 //     logo.style.display = 'none';
 //  });
 
-close.addEventListener('click', ()=> {
-window.location = 'goodlifeWithLifestyleBlogs.html'
-});
+// close.addEventListener('click', ()=> {
+// window.location = 'goodlifeWithLifestyleBlogs.html'
+// });
 
 
 
-back.addEventListener('click',()=>{
-    window.location= 'goodlifeWithLifestyleBlogs.html';
+// back.addEventListener('click',()=>{
+//     window.location= 'goodlifeWithLifestyleBlogs.html';
+// });
+
+const PopUp = document.querySelector('.forForm') 
+const Pop = document.querySelector('.RegClass');
+const head = document.querySelector('.header')
+const content = document.querySelector('.imagesSec')
+const foot = document.querySelector('.footer');
+
+PopUp.style.display = "block";
+head.style.display = 'none';
+content.style.display = 'none';
+foot.style.display = 'none';
+
+Pop.addEventListener('click',()=>{
+	PopUp.style.display = "none";
+	head.style.display = 'block';
+content.style.display = 'block';
+foot.style.display = 'block';
+
 });
 
 
